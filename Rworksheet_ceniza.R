a5d958381f218926991d1af94b6ebeea6137bc03
ZydrickCeniza <zydrick.ceniza@gmail.com>
2023-09-23 14:02:04 +0000

    ZydrickCeniza_BSIT2B_CS101

--- Rworksheet_ceniza.R
+++ Rworksheet_ceniza.R
@@ -3,11 +3,10 @@
 #51, 35, 24, 33, 41.
 age<- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
 age
-#a. How many data points?
+
+#a. How many data points? 34
 length(age)
 #[1] 34
-
-
 
 #2. Find the reciprocal of the values for age.
 reciprocal_age <- 1 / age
@@ -18,79 +17,68 @@ reciprocal_age
 #[25] 0.05882353 0.02702703 0.02380952 0.01886792 0.02439024 0.01960784 0.02857143 0.04166667
 #[33] 0.03030303 0.02439024
 
-
-
 #3. Assign also new_age <- c(age, 0, age).
-new_age <- c(age, 0, age)
+new_age <- c(age,0, age)
 new_age
 #[1] 34 28 22 36 27 18 52 39 42 29 35 31 27 22 37 34 19 20 57 49 50 37 46 25 17 37 42 53 41 51
 #[31] 35 24 33 41  0 34 28 22 36 27 18 52 39 42 29 35 31 27 22 37 34 19 20 57 49 50 37 46 25 17
 #[61] 37 42 53 41 51 35 24 33 41
-
-
 
 #4. Sort the values for age.
 sort(age)
 #[1] 17 18 19 20 22 22 24 25 27 27 28 29 31 33 34 34 35 35 36 37 37 37 39 41 41 42 42
 #[28] 46 49 50 51 52 53 57
 
-
-
-#5. Find the minimum and maximum value for age.
+#5. Find the minimum and maximum value for age. min is 17 max is 57
 min(age)
 #[1] 17
 max(age)
 #[1] 57
-
-
 
 #6. Set up a vector named data, consisting of 2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
 #2.5, 2.3, 2.4, and 2.7.
 data<- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
 data
 #[1] 2.4 2.8 2.1 2.5 2.4 2.2 2.5 2.3 2.5 2.3 2.4 2.7
-#a. How many data points?
+#a. How many data points? 12
 length(data)
 #[1] 12
 
 
-
-#7. Generates a new vector for data where you double every value of the data. | What happen
-#to the data?
+#7. Generates a new vector for data where you double every value of the data. | 
+#What happen to the data?
 doubledata <- data*2
 doubledata
 #[1] 4.8 5.6 4.2 5.0 4.8 4.4 5.0 4.6 5.0 4.6 4.8 5.4
 
+# 8. Generate a sequence for the following scenario:
+#   8.1 Integers from 1 to 100.
+num8_1<-seq(1,100,by=1)
+# [1]   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22
+# [23]  23  24  25  26  27  28  29  30  31  32  33  34  35  36  37  38  39  40  41  42  43  44
+# [45]  45  46  47  48  49  50  51  52  53  54  55  56  57  58  59  60  61  62  63  64  65  66
+# [67]  67  68  69  70  71  72  73  74  75  76  77  78  79  80  81  82  83  84  85  86  87  88
+# [89]  89  90  91  92  93  94  95  96  97  98  99 100
 
 
-#8. Generate a sequence for the following scenario:
-#8.1 Integers from 1 to 100.
-seq(1,100, by=1)
-#[1]   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22
-#[23]  23  24  25  26  27  28  29  30  31  32  33  34  35  36  37  38  39  40  41  42  43  44
-#[45]  45  46  47  48  49  50  51  52  53  54  55  56  57  58  59  60  61  62  63  64  65  66
-#[67]  67  68  69  70  71  72  73  74  75  76  77  78  79  80  81  82  83  84  85  86  87  88
-#[89]  89  90  91  92  93  94  95  96  97  98  99 100
+# 8.2 Numbers from 20 to 60
+num8_2<-seq(20,60,by=1)
+# [1] 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49
+# [31] 50 51 52 53 54 55 56 57 58 59 60
 
-#8.2 Numbers from 20 to 60
-seq(20,60, by=1)
-#[1] 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49
-#[31] 50 51 52 53 54 55 56 57 58 59 60
+# *8.3 Mean of numbers from 20 to 60
+num8_3<-seq(20,60,by=1)
+mean(num8_3)
+# [1] 40
 
-#8.3 Mean of numbers from 20 to 60
-numbers <- seq(20, 60)
-numbers
-mean_value <- mean(numbers)
-mean_value
-#[1] 40
-#8.4 Sum of numbers from 51 to 91
-num84<- seq(51, 91)
-num84
-total_sum <- sum(num84)
-total_sum
-#[1] 2911
-#8.5 Integers from 1 to 1,000
-seq(1,1000)
+# *8.4 Sum of numbers from 51 to 91
+num8_4<-seq(51,91,by=1)
+sum(num8_4)
+# [1] 2911
+
+# *8.5 Integers from 1 to 1,000
+num8_5<-seq(1,1000,by=1)
+num8_5
 # [1]    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17
 # [18]   18   19   20   21   22   23   24   25   26   27   28   29   30   31   32   33   34
 # [35]   35   36   37   38   39   40   41   42   43   44   45   46   47   48   49   50   51
@@ -152,5 +140,91 @@ seq(1,1000)
 # [987]  987  988  989  990  991  992  993  994  995  996  997  998  999 1000
 
 # a. How many data points from 8.1 to 8.4?_______
-length(seq(1,100, by=1))
+#8.1
+length(num8_1)
+# [1] 100
 
+#8.2
+length(num8_2)
+# [1] 41
+
+#8.3
+length(num8_3)
+# [1] 41
+
+#8.4
+length(num8_4)
+# [1] 41
+
+# data points from 8.1 to 8.4 is 224
+length(num8_1)+length(num8_2)+length(num8_3)+length(num8_4)
+# [1] 223
+
+# c. For 8.5 find only maximum data points until 10.
+
+# 9. *Print a vector with the integers between 1 and 100 that are not divisible by 3, 5 and 7
+# using filter option.
+# filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100))
+# Write the R code and its output.
+
+
+
+# 10. Generate a sequence backwards of the integers from 1 to 100.
+num10<-seq(100,1,by=-1)
+num10  
+# [1] 100  99  98  97  96  95  94  93  92  91  90  89  88  87  86  85  84  83  82  81  80  79  78  77  76  75
+# [27]  74  73  72  71  70  69  68  67  66  65  64  63  62  61  60  59  58  57  56  55  54  53  52  51  50  49
+# [53]  48  47  46  45  44  43  42  41  40  39  38  37  36  35  34  33  32  31  30  29  28  27  26  25  24  23
+# [79]  22  21  20  19  18  17  16  15  14  13  12  11  10   9   8   7   6   5   4   3   2   1
+
+#11. List all the natural numbers below 25 that are multiples of 3 or 5.
+num11<-seq(1,25,by=1)
+multiple<-subset(num11, num11%%3==0|num11%%5==0)
+multiple
+# [1]  3  5  6  9 10 12 15 18 20 21 24 25
+
+# a. How many data points from 10 to 11?
+length(num10)+length(multiple)
+# [1] 112
+# data points from 10 to 11 is 112
+
+# 12. Statements can be grouped together using braces ‘{’ and ‘}’. A group of statements
+# is sometimes called a block. Single statements are evaluated when a new line is typed at
+# the end of the syntactically complete statement. Blocks are not evaluated until a new line is
+# entered after the closing brace.
+
+
+# 13. *Set up a vector named score, consisting of 72, 86, 92, 63, 88, 89, 91, 92, 75,
+# 75 and 77. To access individual elements of an atomic vector, one generally uses the x[i]
+# construction.
+score<-c(72, 86, 92, 63, 88, 89, 91, 92, 75, 75, 77)
+score
+score[1]
+# Find x[2] and x[3]. Write the R code and its output.
+score[2]
+score[3]
+
+# 14. *Create a vector a = c(1,2,NA,4,NA,6,7).
+# a. Change the NA to 999 using the codes print(a,na.print="-999").
+a<-c(1,2,NA,4,NA,6,7)
+a[is.na(a)] <- 999
+print(a, na.print = "-999")
+# [1]   1   2 999   4 999   6   7
+
+# 15. A special type of function calls can appear on the left hand side of the assignment
+# operator as in > class(x) <- "foo".
+# Follow the codes below:
+#   name = readline(prompt="Input your name: ")
+# age = readline(prompt="Input your age: ")
+# print(paste("My name is",name, "and I am",age ,"years old."))
+# print(R.version.string)
+x<-{
+  name=readline(prompt = "Input your name: ")
+  age=readline(prompt = "Input your age: ")
+  print(paste("My name is",name, "and I am",age,"yearsold"))
+  print(R.version.string)
+}
+# Input your name: Zydrick
+# Input your age: 19
+# [1] "My name is Zydrick and I am 19 yearsold"
+# [1] "R version 4.3.1 (2023-06-16 ucrt)"
